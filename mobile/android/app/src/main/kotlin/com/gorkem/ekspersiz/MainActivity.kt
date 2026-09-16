c74ba7df95f50a9a20e7c58d96b76a5ac10c0b59
GorkemErtas <gorkemertas2163@hotmail.com>
2026-09-16 16:11:52 +0300

    feat: add store-backed subscription billing with RevenueCat

--- mobile/android/app/src/main/kotlin/com/gorkem/ekspersiz/MainActivity.kt
+++ mobile/android/app/src/main/kotlin/com/gorkem/ekspersiz/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.mobile
+package com.gorkem.ekspersiz
 
 import io.flutter.embedding.android.FlutterActivity
 
--- mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import flutter_secure_storage_darwin
 import geocoding_darwin
 import geolocator_apple
 import package_info_plus
+import purchases_flutter
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -18,5 +19,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GeocodingDarwinPlugin.register(with: registry.registrar(forPlugin: "GeocodingDarwinPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PurchasesFlutterPlugin.register(with: registry.registrar(forPlugin: "PurchasesFlutterPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
